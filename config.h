da1ba1e5a5db1318df16658dd4561c5952be4a6e
smokeythekat <owr5273@gmail.com>
2021-03-20 14:36:12 -0300

    config

--- config.h
+++ config.h
@@ -2,7 +2,8 @@ char* default_editor = "vidd";
 char* program_dir = "~/Documents/Test/";
 
 struct lang_setting lang_settings[] = {
-	{ "c", "gcc %stest_%s.c -o %stest_%s -Wall", "%stest_%s", },
+	{ "c", "gcc %stest_%s.c -o %stest_%s -Wall -lX11 -lm -lpthread", "%stest_%s", },
 	{ "py", "echo \"%s%s%s%s\" > /dev/null", "python3 %stest_%s.py", },
 	{ "java", "echo \"%s%s%s%s\" > /dev/null", "java %stest_%s.java", },
+	{ "jl", "echo \"%s%s%s%s\" > /dev/null", "julia %stest_%s.jl", },
 };
